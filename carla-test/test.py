cb633afa63731fe11993f8e39e2517e21a71c041
ll7 <sailingmaster92@gmx.de>
2022-02-25 11:45:30 +0100

    add segmentation camera Fixes #1

--- carla-test/test.py
+++ carla-test/test.py
@@ -38,12 +38,29 @@ def main():
         
         spectator.set_transform(spectator_transform)
         
+        # create a segmentation camera and attach to vehicle
+        
+        seg_camera_bp = blueprint_library.find('sensor.camera.semantic_segmentation')
+        
+        camera_transform = carla.Transform(carla.Location(z=5.0), carla.Rotation(pitch=-90.0))
+        
+        camera = world.spawn_actor(seg_camera_bp, camera_transform, attach_to=vehicle)
+        
+        actor_list.append(camera)
+        print('created %s' % camera.type_id)
+        
+        camera.listen(lambda image: image.save_to_disk('./tmp/%s.png' % image.frame_number, carla.ColorConverter.CityScapesPalette))
+        
         time.sleep(10)
         
     finally:
-        print('finally')    
+        print('finally')
+        print(camera)
+        camera.destroy()
         client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
+        print(camera)
         print('done.')
+        
 
     
     
