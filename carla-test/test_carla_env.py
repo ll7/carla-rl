83faf9448a44f25826df5c47f7dca47cb7d195d4
Lennart Luttkus <lennart.luttkus@uni-a.de>
2022-02-28 13:55:05 +0100

    intermediate commit

--- carla-test/test_carla_env.py
+++ carla-test/test_carla_env.py
@@ -8,9 +8,15 @@ import time
 def main():
     env = CarlaWalkerEnv()
     env.reset()
-    env.render()
-    # env.step()
+    # env.render()
+    # env.step(env.action_space.sample())
     time.sleep(5)
+    # env.step(env.action_space.sample())
+    for i in range(1):
+        env.step(env.action_space.sample())
+        logging.debug('step {} in main'.format(i))
+        # env.render()
+        time.sleep(1)
     env.close()
     
     pass
