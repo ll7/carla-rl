7aead0a1cc48de93df07f03e97568bc934db4b9c
Lennart Luttkus <lennart.luttkus@uni-a.de>
2022-04-26 14:21:53 +0200

    intermediate result #19

--- walker-env/carla_walker_env.py
+++ walker-env/carla_walker_env.py
@@ -21,18 +21,28 @@ def read_IP_from_file(file_name="ip-host.txt"):
 
 
 class CarlaWalkerEnv(Env):
-    """simple walk gym environment for carla where you try to walk as far as possible
+    """
+    simple walk gym environment for carla where you try to walk as far as possible
 
     https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html
     """
 
     def __init__(
         self,
-        render=True,
-        verbose=True,
-        host='localhost',
+        render: bool = True,
+        verbose: bool = True,
+        host: str = 'localhost',
+        town: str = 'Town01',
     ):
         """
+        initializes the CarlaWalkerEnv Gym Environment
+
+        Args:
+            render (bool, optional): should the spectator view move with the vehicle. Defaults to True.
+            verbose (bool, optional): Log everything. Defaults to True.
+            host (str, optional): IP address of the carla server. Defaults to 'localhost'.
+            town (str, optional): town to load. Defaults to 'Town01'.
+
         init:
         - define default values
         - define interface 
@@ -41,7 +51,7 @@ class CarlaWalkerEnv(Env):
         - spawn walker
         - attach camera
         """
-        
+
         super(CarlaWalkerEnv, self).__init__()
 
         # set constant parameters
@@ -49,49 +59,102 @@ class CarlaWalkerEnv(Env):
         self.verbose = verbose
         self.host = host
 
-        self.slow_step = False  # sleep each step to show simulation in real time
+        self.slow_step = False
+        """sleep each step to show simulation in real time
+        
+        bad for training but good for debugging"""
 
-        self.town = 'Town01'
+        self.town = town
         self.image_size_x = 32
         self.image_size_y = 32
-        self.pov = 170.0
+        self.fov = 170.0
+        """field of view of the camera"""
 
-        self.max_tick_count = 20*20
         self.fixed_time_step = 0.05
+        """fixed time step size in seconds
+        used in synchronous mode"""
+
+        self.max_tick_count = 20 * (1/self.fixed_time_step)
+        """max number of ticks per episode"""
 
         self.max_walking_speed = 15.0 / 3.6  # m/s
+        """max walking speed of the walker"""
 
         self.now = datetime.now().strftime('%Y-%m-%d_%H%M')
 
         self.actor_list = []
-        
-        # # Space Definiton
+        """stores all actors that are created"""
 
-        # this needs to be normalized later to a total length of 1
+        # === Space Definiton ===
+
         self.action_space = Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        """this needs to be normalized later to a total length of 1"""
 
-        # receive a segmentation image from the carla server
-        # https://carla.readthedocs.io/en/latest/ref_sensors/#semantic-segmentation-camera
+        self.max_dist_start = 100.0
+        """maximum distance to walk in one episode for observation space
+        TODO check max_dist_start in episode end"""
+
         self.observation_space = Dict({
-            "segmentation_camera": 
+            "segmentation_camera":
                 Box(
                     low=0,
                     high=22,  # 22 is the number of classes in the segmentation image
                     shape=(self.image_size_x, self.image_size_y, ),
                     dtype=np.uint8
                 ),
-            "start_location": Box(low=-1, high=1, shape=(2,), dtype=np.float32), # TODO fix low and high
-            "current_location": Box(low=-1, high=1, shape=(2,), dtype=np.float32), # TODO fix low and high
+            "distance_from_start": Box(
+                low=-self.max_dist_start,
+                high=self.max_dist_start,
+                shape=(2,),
+                dtype=np.float32
+                ),
+            # TODO fix low and high and shape
+            "current_orientation": Box(low=-1, high=1, shape=(2,), dtype=np.float32),
         })
-        # TODO fix observation space everywhere else.
+        """
+        observation_space: gym.spaces.Dict
         
-        # TODO Test: initialize observation by sampling from the observation space
+        receive a segmentation image from the carla server
+        https://carla.readthedocs.io/en/latest/ref_sensors/#semantic-segmentation-camera
+        """
+
+        # TODO fix observation space everywhere else.
+
+        # initialize observation by sampling from the observation space
         self.observation = self.observation_space.sample()
-        # np.ndarray(
-        #     shape=(self.image_size_x, self.image_size_y, ), dtype=np.uint8)
 
-        # carla setup
+        self.__setup_carla()
 
+        # === walker ===
+        self.__spawn_walker()
+
+        if self.verbose:
+            # print distance to spawn point
+            # TODO obsolete?
+            distance_from_spawn_point = self.walker.get_location() - \
+                self.walker_spawn_transform.location
+            logging.debug('distance from spawn point: %s' %
+                          distance_from_spawn_point.length())
+
+        # === camera ===
+        self.__create_camera()
+
+        # create segmentation camera listener which is called each tick and updates observation
+        self.camera.listen(
+            lambda data: self.__create_observation(data)
+        )
+
+        # tick once for the changes to take effect
+        self.world.tick()
+
+    def __setup_carla(self):
+        """
+        setup carla server:
+
+        - connect the client to the carla server
+        - load the map
+        - set synchronous mode
+        """
         logging.debug('waiting for server')
         logging.debug('try to connect to server %s' % self.host)
         self.client = carla.Client(self.host, 2000)
@@ -129,32 +192,6 @@ class CarlaWalkerEnv(Env):
 
         self.blueprint_library = self.world.get_blueprint_library()
 
-        # === walker ===
-        self.__spawn_walker()
-
-        if self.verbose:
-            # print distance to spawn point
-            # TODO obsolete?
-            distance_from_spawn_point = self.walker.get_location() - \
-                self.walker_spawn_transform.location
-            logging.debug('distance from spawn point: %s' %
-                          distance_from_spawn_point.length())
-
-        # === camera ===
-        self.__create_camera()
-
-        # create segmentation camera listener which is called each tick and updates observation
-        self.camera.listen(
-            lambda data: self.__create_observation(data)
-            # lambda image: image.save_to_disk(
-            #     './tmp/{}/{}.png'.format(self.now, image.frame_number),
-            #     carla.ColorConverter.CityScapesPalette
-            #     )
-        )
-
-        # tick once for the changes to take effect
-        self.world.tick()
-
     def __create_camera(self):
         """create a camera and attach it to the walker"""
 
@@ -170,7 +207,7 @@ class CarlaWalkerEnv(Env):
             'image_size_x', str(self.image_size_x))
         self.seg_camera_bp.set_attribute(
             'image_size_y', str(self.image_size_y))
-        self.seg_camera_bp.set_attribute('fov', str(self.pov))
+        self.seg_camera_bp.set_attribute('fov', str(self.fov))
 
         self.camera = self.world.spawn_actor(
             self.seg_camera_bp,
@@ -192,8 +229,11 @@ class CarlaWalkerEnv(Env):
 
         pass
 
-    def __create_observation(self, data):
+    def __create_observation(self, data: carla.Image):
         """create observation based on the camera image received
+
+        Args:
+            data (carla.Image): Image from the data
 
         the data we receive is a carla.Image object formatted as a bgra byte array
 
@@ -205,13 +245,8 @@ class CarlaWalkerEnv(Env):
         to get our observation we access the array with [start:end:step]
         and we finally reshape the array from 1D to 2D + R channel
         """
+
         logging.debug('=== creating observation ===')
-        # logging.debug('data: {}'.format(data))
-        # logging.debug('data type: {}'.format(type(data)))
-        # logging.debug('data len: {}'.format(len(data)))
-        # with open("data_raw_dat" + self.now + ".txt", "w") as text_file:
-        #     text_file.write(str(data.raw_data))
-        # data.convert(carla.ColorConverter.Raw)
 
         if self.verbose:
             data.save_to_disk('./tmp/{}/{}.png'.format(self.now,
@@ -219,36 +254,39 @@ class CarlaWalkerEnv(Env):
             logging.debug('image saved to disk')
         only_red = np.array(data.raw_data, dtype=np.uint8)[
             2::4]  # only red values from bgRa
+        
         # reshape. validation: https://github.com/ll7/carla-rl/issues/7#issuecomment-1055582311
-        self.observation = only_red.reshape(
-            self.image_size_x, self.image_size_y)
-
-        # self.observation = np.array(data.raw_data, dtype=np.uint8)[2::4] # only red values from bgRa
-        # np.array(data.raw_data)
-        # logging.debug('observation: {}'.format(self.observation))
-        # logging.debug('observation type: {}'.format(type(self.observation)))
-        # logging.debug('observation len: {}'.format(len(self.observation)))
-        # with open("observation" + self.now + ".txt", "w") as text_file:
-        #     text_file.write(str(self.observation))
-
-        # np.savetxt("observation" + self.now + ".txt",
-        #            self.observation, fmt='%d')
-
-        # data.convert(carla.ColorConverter.Raw)
-        # print(data)
-        # print(len(data))
-        # print(type(data))
+        self.observation["segmentation_camera"] = only_red.reshape(
+            self.image_size_x,
+            self.image_size_y
+        )
+        
+        # TODO TEST if yaw is correct angle for orientation
+        self.observation["current_orientation"] = self.walker.get_transform().rotation.yaw
+        
+        # TODO TEST if transform is recieved correctly
+        # TODO location is 3D, observation is 2D.
+        vec_from_spawn2walker = self.walker.get_transform().location - self.walker_spawn_transform.location
+        
+        self.observation["distance_from_start"] = self.walker.get_location() - \
+            self.walker_spawn_transform.location
+        
 
         logging.debug('=== observation created ===')
 
     def __reward_calculation(self):
         """calculate the reward and apply reward
         """
-        # distance_vector = self.walker_spawn_transform - self.walker.get_location()
         self.reward = self.walker_spawn_transform.location.distance(
             self.walker.get_location())
 
     def __spawn_walker(self):
+        """Spawn the walker in a random vehicle spawn point
+
+        TODO make it possible to spawn the walker in a specific spawn point
+        TODO choose a random spawn poitn for a **walker**        
+        """
+
         self.walker_bp = self.blueprint_library.filter('0012')[0]
 
         spawn_points = self.world.get_map().get_spawn_points()
@@ -298,20 +336,37 @@ class CarlaWalkerEnv(Env):
 
         return self.observation
 
-    def step(self, action):
-        """apply action to walker and return reward, observation, done, info
+    def step(self, action: np.ndarray):
+        """
+        apply action to walker and return observation, reward, done, info
 
+        Parameters
+        ----------
+        action : np.ndarray
+            action of the walker
+
+        Returns
+        -------
+        observation, reward, done, info : list
+            _description_
+
+        Example
+        -------
         action: array([-0.612514  ,  0.95657253], dtype=float32)
             shape: (2,)
 
-        #carlawalkercontrol
+        ## Background
+
+        carlawalkercontrol
         applies carla.WalkerControl https://carla.readthedocs.io/en/latest/python_api/
         """
+
         logging.debug('taking step')
 
         # self.walker_last_location = self.walker.get_location()
 
-        # length of the action vector could effect the maximum speed of the walker which is not desired and this special case is catched here
+        # length of the action vector could effect the maximum speed of the walker
+        # which is not desired and this special case is catched here
         action_length = np.linalg.norm(action)
         if action_length == 0.0:
             # the chances are slim, but theoretically both actions could be 0.0
@@ -346,7 +401,6 @@ class CarlaWalkerEnv(Env):
         if self.slow_step == True:
             time.sleep(self.fixed_time_step)
 
-        # TODO return
         return self.observation, self.reward, self.done, self.info
 
     def render(self, init=False):
